a2037413a0290ae483045bb823ac859e275f8b33
sandakelum priyamantha <i.g.sandakelum@gmail.com>
2020-08-05 12:45:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ class App{
         c_name.innerHTML = name
         tbody.innerHTML = "<tr><td>"+data.confirmed.value+"</td><td>"+data.recovered.value+"</td><td>"+data.deaths.value+"</td></tr>"
         let p = data.confirmed.value-(data.recovered.value+data.deaths.value)
-        chart.src = "https://image-charts.com/chart?cht=pd&chd=t:"+p+","+data.recovered.value+","+data.deaths.value+"&chs=700x190&chdl="+str.p+"|"+str.s+"|"+str.m+"&chli="+data.confirmed.value
+        chart.src = "https://image-charts.com/chart?cht=pd&chd=a:"+p+","+data.recovered.value+","+data.deaths.value+"&chs=700x190&chdl="+str.p+"|"+str.s+"|"+str.m+"&chli="+data.confirmed.value
 
     }
 }
